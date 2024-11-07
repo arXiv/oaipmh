2a5635a14e3068cff324d9a1f016bfc0e9013216
kyokukou <erin@arxiv.org>
2024-11-07 09:25:03 -0800

    checking params for list records

--- oaipmh/requests/data_queries.py
+++ oaipmh/requests/data_queries.py
@@ -20,7 +20,26 @@ def get_record(params: Dict[str, str]) -> Response:
     return "<a>b</a>", 200, {}
 
 def list_records(params: Dict[str, str]) -> Response:
+    """used to harvest records from a repository with support for selective harvesting"""
 
+    token=None
+    #get parameters
+    given_params=set(params.keys())
+    if OAIParams.RES_TOKEN in given_params:
+        if given_params != {OAIParams.RES_TOKEN, OAIParams.VERB}: #resumption token is exclusive
+            raise OAIBadArgument
+        token=params[OAIParams.RES_TOKEN]
+    else:
+        if OAIParams.META_PREFIX not in given_params:
+            raise OAIBadArgument
+        allowed_params={OAIParams.VERB,OAIParams.META_PREFIX, OAIParams.FROM, OAIParams.UNTIL, OAIParams.SET }
+        if given_params-allowed_params: #no extra keys allowed
+            raise OAIBadArgument
+
+        meta_type=params[OAIParams.META_PREFIX]
+        from_str=params.get(OAIParams.FROM)
+        until_str=params.get(OAIParams.UNTIL)
+        set_str=params.get(OAIParams.SET)
     #TODO rest of function
 
     return "<a>b</a>", 200, {}
