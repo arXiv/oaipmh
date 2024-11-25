a3514dbd61cf5399af0546e7788b05af7405c8c9
kyokukou <erin@arxiv.org>
2024-11-25 08:54:37 -0800

    combine get record queries into one

--- oaipmh/processors/db.py
+++ oaipmh/processors/db.py
@@ -1,23 +1,26 @@
-from typing import Optional, List
+from typing import Optional, List, Union
+from sqlalchemy.orm import aliased
 
 from arxiv.db import Session
 from arxiv.db.models import Metadata
 from arxiv.identifier import Identifier
 
 
-def get_record_data_current(arxiv_id: Identifier )-> Optional[Metadata]:
-    """fetch latest metadata for a specific paper"""
-    data=(Session.query(Metadata)
-          .filter(Metadata.paper_id == arxiv_id.id)
-          .filter(Metadata.is_current==1)
-          .first()
+def get_record_data(arxiv_id: Identifier, all_versions:bool )-> Optional[Union[Metadata, List[Metadata]]]:
+    """fetch metadata for a specific paper"""
+    data_subquery = (
+        Session.query(Metadata)
+        .filter(Metadata.paper_id == arxiv_id.id)
+        .subquery()
     )
-    return data
-    
-def get_record_data_all(arxiv_id: Identifier)-> Optional[List[Metadata]]:
-    """fetch all metadata for a specific paper"""
-    data=(Session.query(Metadata)
-          .filter(Metadata.paper_id == arxiv_id.id)
-          .all()
-    )
-    return data
+
+    metadata_alias = aliased(Metadata, data_subquery)
+    if all_versions:
+        result = Session.query(metadata_alias).all()
+    else:
+        result = (
+            Session.query(metadata_alias)
+            .filter(metadata_alias.is_current == 1)
+            .first()
+        )
+    return result
