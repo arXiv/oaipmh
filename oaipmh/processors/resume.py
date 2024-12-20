71fa39646a8f1f1ccb7235f4dadafa59f611b82c
kyokukou <erin@arxiv.org>
2024-12-20 09:46:52 -0800

    url safe, shorter token

--- oaipmh/processors/resume.py
+++ oaipmh/processors/resume.py
@@ -1,6 +1,5 @@
 from typing import Dict, Tuple
-import json
-import base64
+from urllib.parse import urlencode, quote, unquote, parse_qs
 
 from oaipmh.data.oai_errors import OAIBadResumptionToken
 from oaipmh.data.oai_properties import OAIParams
@@ -12,26 +11,22 @@ class ResToken:
         self.token_str = self.to_token()
         #TODO expire on next announce
 
-    def to_token(self) -> str: #TODO encode special characters
+    def to_token(self) -> str: 
         params = self.params.copy()
         params.pop("resumptionToken", None)
-        data = {
-            "params": params,
-            "start_val": self.start_val
-        }
-        json_str = json.dumps(data)
-        return base64.b64encode(json_str.encode("utf-8")).decode("utf-8")
+        params["skip"]=self.start_val
+        return quote(urlencode(params))
 
     @classmethod
     def from_token(cls, encoded_str: str) -> Tuple[Dict[str, str], int]:
         try:
-            json_str = base64.b64decode(encoded_str).decode("utf-8")
-            data = json.loads(json_str)
-            if not isinstance(data, dict) or set(data.keys()) != {"params", "start_val"}:
+            decoded_str = unquote(encoded_str)
+            parsed_params = parse_qs(decoded_str)
+            params = {key: value[0] for key, value in parsed_params.items()}
+            if "skip" not in params or not params["skip"].isdigit():
                 raise OAIBadResumptionToken("Token structure is invalid.")
-            if not isinstance(data["params"], dict) or not isinstance(data["start_val"], int):
-                raise OAIBadResumptionToken("Token contains invalid data types.")
-            return data["params"], data["start_val"]
+            start_val = int(params.pop("skip"))
+            return params, start_val
         except (Exception):
             raise OAIBadResumptionToken("Token decoding failed or format is invalid.")
 
