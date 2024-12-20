518b4a7ced12b499504101abc9d1764ccaa3ff1b
kyokukou <erin@arxiv.org>
2024-12-20 11:23:35 -0800

    update token testing

--- tests/test_token.py
+++ tests/test_token.py
@@ -1,72 +1,69 @@
 
 import base64
 import json
+from datetime import datetime
 import pytest
+from urllib.parse import urlencode, quote, unquote, parse_qs
 
 from oaipmh.data.oai_errors import OAIBadResumptionToken
+from oaipmh.data.oai_properties import OAIParams, OAIVerbs
 from oaipmh.processors.resume import ResToken
 
 def test_token_encoding_decoding():
-    params = {"verb": "GetRecord", "identifier": "12345"}
-    res_token = ResToken(params=params, start_val=10)
+    unallowed_chars=['/','?','#','=','&',':',';',' ','+']
+
+    #minimal params
+    params = {OAIParams.VERB: OAIVerbs.LIST_RECORDS, OAIParams.META_PREFIX:'oai_dc'}
+    res_token = ResToken(params=params, start_val=0)
     assert res_token.params== params
-    assert res_token.start_val == 10
+    assert res_token.start_val == 0
     encoded_token = res_token.to_token()
     assert isinstance(encoded_token, str)
+    assert not any(char in encoded_token for char in unallowed_chars)
 
     dict, num = ResToken.from_token(encoded_token)
     assert dict == params
-    assert num == 10
+    assert num == 0
 
+    #maximal params
+    params = {OAIParams.VERB: OAIVerbs.LIST_IDS, OAIParams.META_PREFIX:'arXiv', OAIParams.FROM:'2011-10-08', OAIParams.UNTIL:'2013-10-08', OAIParams.SET:"math:math.NA"}
+    res_token = ResToken(params=params, start_val=33)
+    assert res_token.params== params
+    assert res_token.start_val == 33
+    encoded_token = res_token.to_token()
+    assert isinstance(encoded_token, str)
+    assert not any(char in encoded_token for char in unallowed_chars)
+
+    dict, num = ResToken.from_token(encoded_token)
+    assert dict == params
+    assert num ==33
 
 def test_invalid_token_structure():
-    invalid_data = {
-        "not_params": {"verb": "GetRecord"},  
-        "start_val": 10
-    }
-    encoded_invalid_token = base64.b64encode(json.dumps(invalid_data).encode("utf-8")).decode("utf-8")
+    params = {OAIParams.VERB: OAIVerbs.LIST_RECORDS, OAIParams.META_PREFIX:'oai_dc', 'skip_val':55}
+    encoded_invalid_token =quote(urlencode(params))
     with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid."):
         ResToken.from_token(encoded_invalid_token)
-
 
 def test_invalid_token_data_types():
-    invalid_data = {
-        "params": "not a dict", 
-        "start_val": "not an int"  
-    }
-    encoded_invalid_token = base64.b64encode(json.dumps(invalid_data).encode("utf-8")).decode("utf-8")
+    params = {OAIParams.VERB: OAIVerbs.LIST_RECORDS, OAIParams.META_PREFIX:'oai_dc', 'skip_val':datetime(2023,9,1)}
+    encoded_invalid_token =quote(urlencode(params))
     with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid."):
         ResToken.from_token(encoded_invalid_token)
 
-
 def test_token_decoding_random_str():
-    malformed_base64 = "not_a_valid_base64_string"
+    random_str = "twinkletwinklelittlestar"
     with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid"):
-        ResToken.from_token(malformed_base64)
+        ResToken.from_token(random_str)
 
-
-def test_token_decoding_with_invalid_json():
-    invalid_json_base64 = base64.b64encode(b"not valid json").decode("utf-8")
+def test_token_decoding_with_invalid_encoding():
     with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid"):
-        ResToken.from_token(invalid_json_base64)
+        ResToken.from_token("?verb=ListIdentifiers&metadataPrefix=arXiv")
 
     with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid"):
         ResToken.from_token("")
 
-
-def test_extra_or_missing_keys_in_token():
-    extra_key_data = {
-        "params": {"verb": "GetRecord", "identifier": "12345"},
-        "start_val": 10,
-        "extra_key": "unexpected"
-    }
-    encoded_extra_key = base64.b64encode(json.dumps(extra_key_data).encode("utf-8")).decode("utf-8")
+def test_missing_key_in_token():
+    params = {OAIParams.VERB: OAIVerbs.LIST_RECORDS, OAIParams.META_PREFIX:'oai_dc'}
+    encoded_invalid_token =quote(urlencode(params))
     with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid."):
-        ResToken.from_token(encoded_extra_key)
-    
-    missing_key_data = {
-        "params": {"verb": "GetRecord"}
-    }
-    encoded_missing_key = base64.b64encode(json.dumps(missing_key_data).encode("utf-8")).decode("utf-8")
-    with pytest.raises(OAIBadResumptionToken, match="Token decoding failed or format is invalid."):
-        ResToken.from_token(encoded_missing_key)
+        ResToken.from_token(encoded_invalid_token)
